7d085e33125cb541ec8b358eff7d091b7b4f11c6
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-05-20 21:57:20 +0530

    feat: implement auth-require middleware

---
+++ src/middlewares/auth-require.middleware.ts
@@ -0,0 +1,32 @@
+import { ErrorCode } from "@/enums/error-code.enum";
+import { UnauthorizedError } from "@/errors/unauthorize.error";
+import { verifyJwt } from "@/utils/jwt";
+import { Request, Response, NextFunction } from "express";
+
+export const authMiddleware = (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token)
+      throw new UnauthorizedError(
+        "Authentication token not found",
+        ErrorCode.AUTH_TOKEN_NOT_FOUND,
+      );
+
+    const { payload, error } = verifyJwt(token);
+    if (error || !payload) {
+      throw new UnauthorizedError(
+        "Invalid or expired authentication token",
+        ErrorCode.AUTH_INVALID_TOKEN,
+      );
+    }
+
+    req.user = { id: payload.userId };
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
